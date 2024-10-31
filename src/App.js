37ff4c52c6531bece138ff626021f8345ace5aff
Sai-Karthik9113 <sai9898karthik@gmail.com>
2024-10-31 15:06:18 +0530

    feat: update SpellCheck with functional-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import SpellCheckApp from './SpellCheck';
 
 function App() {
   return (
     <div className="App">
+      <SpellCheckApp />
     </div>
   );
 }
